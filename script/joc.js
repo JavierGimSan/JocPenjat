cc80309cf6152047e36fbaf2ee5e10a2ae3e7cc4
JavierGimSan <javi.gimsa@gmail.com>
2024-10-17 16:53:45 +0200

    Funció de jugar lletra completada

--- script/joc.js
+++ script/joc.js
@@ -1,23 +1,30 @@
 
 //Objectes
-const inpuObj = document.getElementById("paraulaSecreta");
+const inpuObj = document.getElementById("paraulaIntroduida");
 const buttonObj = document.getElementById("comencarPartida");
+const imgObj = document.getElementById("imatge");
+const paraulaActualObj = document.getElementById("titolComencar");
 
 //Variables
-
+let paraulaIntroduida;
 let paraulaSecreta;
+let paraulaActual = [];
+let jugadaFallada = 0;
+let comptador = 0;
 
-
+deshabilitarBoto();
 
 function comencarPartida(){
-
-    paraulaSecreta = inpuObj.value;
-
-    if(paraulaSecreta){
-        if(isNaN(paraulaSecreta)){
-            if(paraulaSecreta.length > 3){
-                console.log(paraulaSecreta);
-                console.log(paraulaSecreta.split("")); //Separa l'string pel criteri que volguem. En aquest cas, separa TOTS els caracters.
+    paraulaIntroduida = inpuObj.value.toUpperCase();    
+    if(paraulaIntroduida){
+        if(isNaN(paraulaIntroduida)){
+            if(paraulaIntroduida.length > 3){
+                habilitarBoto();
+                paraulaSecreta = paraulaIntroduida.split("");//Separa l'string pel criteri que volguem. En aquest cas, separa TOTS els caracters.
+                paraulaActualInicial();
+                mostrarParaulaPantalla();
+                console.log(paraulaIntroduida);
+                console.log(paraulaSecreta); 
                 inpuObj.disabled=true; //Desactiva que poguem introduir res.
                 buttonObj.disabled=true; //Desactiva el botó.
     
@@ -32,6 +39,17 @@ function comencarPartida(){
     }    
 }
 
+function paraulaActualInicial(){
+    for(let i = 0; i < paraulaSecreta.length; i++){
+        // paraulaActual[i] = "_"; Queda molt retro. Millor fer aixó:
+        paraulaActual.push("_");
+    }
+}
+
+function mostrarParaulaPantalla(){
+    paraulaActualObj.textContent = paraulaActual.toString().replaceAll(",", " ");
+}
+
 function mostrarParaula(){
     if(inpuObj.type==="password"){
         inpuObj.type="text";
@@ -39,3 +57,37 @@ function mostrarParaula(){
         inpuObj.type="password";
     }    
 }
+
+function jugarLletra(obj){
+    let lletraJugada = obj.textContent;
+    for (i=0; i<paraulaSecreta.length; i++){ //Index per recorrer la paraula a endevinar.
+        if(paraulaSecreta[i]==lletraJugada){ //Si la lletra jugada és la mateixa que la de l'index de la paraula a endevinar...
+            paraulaActual[i] = lletraJugada; //...es copia a la paraula nova que es mostrarà per pantalla(paraulaActual)
+            mostrarParaulaPantalla();
+        }
+    }
+    comptador = comptador +1;
+    console.log(comptador);
+    console.log(lletraJugada);
+}
+
+jugarLletra();
+
+function deshabilitarBoto(){
+    for(let i = 1; i < 27; i++){
+        let literal = "boto_" + i;
+        const boto = document.getElementById(literal);
+        boto.disabled = true;
+    }
+}
+
+function habilitarBoto(){
+    for(let i = 1; i < 27; i++){
+        let literal = "boto_" + i;
+        const boto = document.getElementById(literal);
+        boto.disabled = false;
+    }
+}
+
+
+
